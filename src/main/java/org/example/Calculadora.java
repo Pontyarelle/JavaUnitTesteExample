71e46e60543c022bca0ac1b7d8fa0a767230b332
Pontyarelle <pontyarelle.ads@hotmail.com>
2023-03-29 18:51:00 -0300

    Codigo melhorado,testado  e finalizado

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -3,51 +3,21 @@ import java.util.Scanner;
 
 public class Calculadora {
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
-
-        double num1, num2, resultado;
+        double num1, num2;
         char operador;
-
         System.out.print("Digite o primeiro número: ");
         num1 = input.nextDouble();
-
         System.out.print("Digite o segundo número: ");
         num2 = input.nextDouble();
-
         System.out.print("Digite o operador (+, -, *, /): ");
         operador = input.next().charAt(0);
-
-        switch (operador) {
-            case '+':
-                resultado = num1 + num2;
-                System.out.printf("%.2f + %.2f = %.2f", num1, num2, resultado);
-                break;
-            case '-':
-                resultado = num1 - num2;
-                System.out.printf("%.2f - %.2f = %.2f", num1, num2, resultado);
-                break;
-            case '*':
-                resultado = num1 * num2;
-                System.out.printf("%.2f * %.2f = %.2f", num1, num2, resultado);
-                break;
-            case '/':
-                if (num2 == 0) {
-                    System.out.println("Erro! Não é possível dividir por zero.");
-                } else {
-                    resultado = num1 / num2;
-                    System.out.printf("%.2f / %.2f = %.2f", num1, num2, resultado);
-                }
-                break;
-            default:
-                System.out.println("Operador inválido.");
-                break;
-        }
-
+        double resultado = calcular(num1, num2, operador);
+        System.out.printf("%.2f %c %.2f = %.2f", num1, operador, num2, resultado);
         input.close();
     }
 
-    public double calcular(double num1, double num2, char operador) {
+    public static double calcular(double num1, double num2, char operador) {
         double resultado = 0.0;
         switch (operador) {
             case '+':
@@ -64,8 +34,11 @@ public class Calculadora {
                     resultado = num1 / num2;
                 }
                 break;
+            default:
+                System.out.println("Operador inválido.");
+                break;
         }
         return resultado;
     }
-
 }
+
