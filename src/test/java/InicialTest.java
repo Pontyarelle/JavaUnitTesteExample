1ecfabd4c4475e03ff1d9a0c7ffb7557bf3035b7
Pontyarelle <pontyarelle.ads@hotmail.com>
2023-03-29 16:58:45 -0300

    Initial Project

---
+++ src/test/java/InicialTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InicialTest {
+    private Assertions Asserrions;
+
+    @Test
+    public void verificarJUnit(){
+        Asserrions.assertTrue (true);
+    }
+}
